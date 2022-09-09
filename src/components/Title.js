e8f2e5384341be423f45dd7b976bebf6ff4d6931
KhoaNggo <princeryin@gmail.com>
2022-09-09 18:34:30 +0700

    Deploy app

--- src/components/Title.js
+++ src/components/Title.js
@@ -4,7 +4,7 @@ export const Title = ({ title = "Bộ sưu tập" }) => {
 	return (
 		<div className='py-10 '>
 			<div className='w-fit mx-auto'>
-				<div className='border p-5'>
+				<div className='border-custom p-5'>
 					<p className='text-[#FFFFFF] text-[22px] leading-8'>
 						{title}
 					</p>
